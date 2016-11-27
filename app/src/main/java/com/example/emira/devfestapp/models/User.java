ee97521fd341e9ef929fed3766f603050d3e4c83
Emira Rziga <emirarziga4@gmail.com>
2016-11-27 06:18:25 +0100

    second commit

---
+++ app/src/main/java/com/example/emira/devfestapp/models/User.java
@@ -0,0 +1,15 @@
+package com.example.emira.devfestapp.models;
+
+
+public class User {
+
+    public String username;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+}
